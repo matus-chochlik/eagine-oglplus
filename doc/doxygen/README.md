497e939bb7947f192b3603fd3cc7d20eb2a0396d
Matus Chochlik <chochlik@gmail.com>
2021-07-14 18:42:08 +0200

    Added doxygen and sphinx documentation sources + updated sub-modules

---
+++ doc/doxygen/README.md
@@ -0,0 +1,3 @@
+This Doxygen documentation configuration and source files are intended to be used
+together with the build system infrastructure in the
+https://github.com/matus-chochlik/eagine-all repository.
---
+++ doc/doxygen/include/oglplus.hpp
@@ -0,0 +1,16 @@
+/// @file
+///
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt
+///
+
+namespace eagine {
+
+/// @brief OpenGL API wrapper code is placed in this namespace.
+namespace oglplus {
+/// @defgroup oglplus OpenGL API wrappers
+}
+
+} // namespace eagine
