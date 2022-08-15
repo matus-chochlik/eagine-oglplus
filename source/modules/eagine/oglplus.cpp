7436954ca4c667c771ee7b9c7475dd8ab8b080b0
Matus Chochlik <chochlik@gmail.com>
2022-08-15 10:30:28 +0200

    Added modularized source code

--- source/modules/eagine/oglplus.cpp
+++ source/modules/eagine/oglplus.cpp
@@ -5,4 +5,7 @@
 /// See accompanying file LICENSE_1_0.txt or copy at
 ///  http://www.boost.org/LICENSE_1_0.txt
 ///
-export module eagine.ecs;
+export module eagine.oglplus;
+
+export import :config;
+export import :enum_types;
