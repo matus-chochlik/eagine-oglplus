d41c4dc00ab16da737025a5c86807267d0dd1115
Matus Chochlik <chochlik@gmail.com>
2022-10-29 09:47:45 +0200

    Removed the oglplus_add_glsl_program macro

--- config/OGLplusBaking.cmake
+++ config/OGLplusBaking.cmake
@@ -3,20 +3,6 @@
 #  See accompanying file LICENSE_1_0.txt or copy at
 #   http://www.boost.org/LICENSE_1_0.txt
 #
-function(oglplus_add_glsl_program PROG_NAME)
-	file(GLOB BAKEPROG "${PROG_NAME}.bakeprog")
-	if("${BAKEPROG}" MATCHES "^.*${PROG_NAME}\.bakeprog$")
-		eagine_add_baked_resource(
-			COMMAND oglplus-bake_program_source
-			PREFIX oglp
-			RES_KIND prog
-			RES_NAME ${PROG_NAME}
-		)
-	else()
-		message(FATAL_ERROR "Invalid input(s) for GLSL program '${PROG_NAME}'")
-	endif()
-endfunction()
-
 function(oglplus_add_texture TEX_NAME)
 	file(GLOB BAKETEX "${TEX_NAME}.*.baketex")
 	if("${BAKETEX}" MATCHES "^.*${TEX_NAME}\.(.*)\.baketex$")
