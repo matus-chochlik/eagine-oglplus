c39e96046c252dcf4ab29593e9ce06fe81b2ea36
Matus Chochlik <chochlik@gmail.com>
2024-01-28 08:56:50 +0100

    Update in copyright notices  + updated sub-modules

--- source/tools/combine_rgb.sh
+++ source/tools/combine_rgb.sh
@@ -2,7 +2,7 @@
 # Copyright Matus Chochlik.
 # Distributed under the Boost Software License, Version 1.0.
 # See accompanying file LICENSE_1_0.txt or copy at
-#  http://www.boost.org/LICENSE_1_0.txt
+# https://www.boost.org/LICENSE_1_0.txt
 #
 in_r=${1:-red.png}
 in_g=${2:-green.png}
