75e05c535dbbe0e2e0338cf42497d6b2c0c80642
Matus Chochlik <chochlik@gmail.com>
2024-04-20 12:51:53 +0200

    Adapted to changes in string conversion in core + updated core sub-module

--- source/modules/eagine/from_string.cpp
+++ source/modules/eagine/from_string.cpp
@@ -19,7 +19,7 @@ namespace eagine {
 template <typename EnumClass>
 auto gl_enum_from_string(
   const string_view src,
-  std::type_identity<EnumClass>) noexcept -> optionally_valid<EnumClass> {
+  std::type_identity<EnumClass> = {}) noexcept -> optionally_valid<EnumClass> {
     if(const auto found{oglplus::gl_enum_by_name(src)}) {
         if(const auto conv{convert_if_fits<EnumClass>(*found)}) {
             return {*conv, true};
@@ -28,102 +28,116 @@ auto gl_enum_from_string(
     return {};
 }
 //------------------------------------------------------------------------------
-export auto from_string(
-  const string_view src,
-  const std::type_identity<oglplus::buffer_target> tid,
-  const default_selector_t) noexcept {
-    return gl_enum_from_string(src, tid);
-}
+export template <>
+struct string_traits<oglplus::buffer_target> {
+    static auto from(const string_view src) noexcept {
+        return gl_enum_from_string(
+          src, std::type_identity<oglplus::buffer_target>{});
+    }
+};
 //------------------------------------------------------------------------------
-export auto from_string(
-  const string_view src,
-  const std::type_identity<oglplus::buffer_usage> tid,
-  const default_selector_t) noexcept {
-    return gl_enum_from_string(src, tid);
-}
+export template <>
+struct string_traits<oglplus::buffer_usage> {
+    static auto from(const string_view src) noexcept {
+        return gl_enum_from_string(
+          src, std::type_identity<oglplus::buffer_usage>{});
+    }
+};
 //------------------------------------------------------------------------------
-export auto from_string(
-  const string_view src,
-  const std::type_identity<oglplus::capability> tid,
-  const default_selector_t) noexcept {
-    return gl_enum_from_string(src, tid);
-}
+export template <>
+struct string_traits<oglplus::capability> {
+    static auto from(const string_view src) noexcept {
+        return gl_enum_from_string(
+          src, std::type_identity<oglplus::capability>{});
+    }
+};
 //------------------------------------------------------------------------------
-export auto from_string(
-  const string_view src,
-  const std::type_identity<oglplus::object_type> tid,
-  const default_selector_t) noexcept {
-    return gl_enum_from_string(src, tid);
-}
+export template <>
+struct string_traits<oglplus::object_type> {
+    static auto from(const string_view src) noexcept {
+        return gl_enum_from_string(
+          src, std::type_identity<oglplus::object_type>{});
+    }
+};
 //------------------------------------------------------------------------------
-export auto from_string(
-  const string_view src,
-  const std::type_identity<oglplus::pixel_data_type> tid,
-  const default_selector_t) noexcept {
-    return gl_enum_from_string(src, tid);
-}
+export template <>
+struct string_traits<oglplus::pixel_data_type> {
+    static auto from(const string_view src) noexcept {
+        return gl_enum_from_string(
+          src, std::type_identity<oglplus::pixel_data_type>{});
+    }
+};
 //------------------------------------------------------------------------------
-export auto from_string(
-  const string_view src,
-  const std::type_identity<oglplus::pixel_internal_format> tid,
-  const default_selector_t) noexcept {
-    return gl_enum_from_string(src, tid);
-}
+export template <>
+struct string_traits<oglplus::pixel_internal_format> {
+    static auto from(const string_view src) noexcept {
+        return gl_enum_from_string(
+          src, std::type_identity<oglplus::pixel_internal_format>{});
+    }
+};
 //------------------------------------------------------------------------------
-export auto from_string(
-  const string_view src,
-  const std::type_identity<oglplus::pixel_format> tid,
-  const default_selector_t) noexcept {
-    return gl_enum_from_string(src, tid);
-}
+export template <>
+struct string_traits<oglplus::pixel_format> {
+    static auto from(const string_view src) noexcept {
+        return gl_enum_from_string(
+          src, std::type_identity<oglplus::pixel_format>{});
+    }
+};
 //------------------------------------------------------------------------------
-export auto from_string(
-  const string_view src,
-  const std::type_identity<oglplus::texture_target> tid,
-  const default_selector_t) noexcept {
-    return gl_enum_from_string(src, tid);
-}
+export template <>
+struct string_traits<oglplus::texture_target> {
+    static auto from(const string_view src) noexcept {
+        return gl_enum_from_string(
+          src, std::type_identity<oglplus::texture_target>{});
+    }
+};
 //------------------------------------------------------------------------------
-export auto from_string(
-  const string_view src,
-  const std::type_identity<oglplus::texture_min_filter> tid,
-  const default_selector_t) noexcept {
-    return gl_enum_from_string(src, tid);
-}
+export template <>
+struct string_traits<oglplus::texture_min_filter> {
+    static auto from(const string_view src) noexcept {
+        return gl_enum_from_string(
+          src, std::type_identity<oglplus::texture_min_filter>{});
+    }
+};
 //------------------------------------------------------------------------------
-export auto from_string(
-  const string_view src,
-  const std::type_identity<oglplus::texture_mag_filter> tid,
-  const default_selector_t) noexcept {
-    return gl_enum_from_string(src, tid);
-}
+export template <>
+struct string_traits<oglplus::texture_mag_filter> {
+    static auto from(const string_view src) noexcept {
+        return gl_enum_from_string(
+          src, std::type_identity<oglplus::texture_mag_filter>{});
+    }
+};
 //------------------------------------------------------------------------------
-export auto from_string(
-  const string_view src,
-  const std::type_identity<oglplus::texture_wrap_mode> tid,
-  const default_selector_t) noexcept {
-    return gl_enum_from_string(src, tid);
-}
+export template <>
+struct string_traits<oglplus::texture_wrap_mode> {
+    static auto from(const string_view src) noexcept {
+        return gl_enum_from_string(
+          src, std::type_identity<oglplus::texture_wrap_mode>{});
+    }
+};
 //------------------------------------------------------------------------------
-export auto from_string(
-  const string_view src,
-  const std::type_identity<oglplus::texture_swizzle_mode> tid,
-  const default_selector_t) noexcept {
-    return gl_enum_from_string(src, tid);
-}
+export template <>
+struct string_traits<oglplus::texture_swizzle_mode> {
+    static auto from(const string_view src) noexcept {
+        return gl_enum_from_string(
+          src, std::type_identity<oglplus::texture_swizzle_mode>{});
+    }
+};
 //------------------------------------------------------------------------------
-export auto from_string(
-  const string_view src,
-  const std::type_identity<oglplus::shader_type> tid,
-  const default_selector_t) noexcept {
-    return gl_enum_from_string(src, tid);
-}
+export template <>
+struct string_traits<oglplus::shader_type> {
+    static auto from(const string_view src) noexcept {
+        return gl_enum_from_string(
+          src, std::type_identity<oglplus::shader_type>{});
+    }
+};
 //------------------------------------------------------------------------------
-export auto from_string(
-  const string_view src,
-  const std::type_identity<oglplus::true_false> tid,
-  const default_selector_t) noexcept {
-    return gl_enum_from_string(src, tid);
-}
+export template <>
+struct string_traits<oglplus::true_false> {
+    static auto from(const string_view src) noexcept {
+        return gl_enum_from_string(
+          src, std::type_identity<oglplus::true_false>{});
+    }
+};
 //------------------------------------------------------------------------------
 } // namespace eagine
