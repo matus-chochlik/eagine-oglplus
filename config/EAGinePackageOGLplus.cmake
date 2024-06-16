91f1f86f4e03acf8e152d7682f7942702e7f3d5a
Matus Chochlik <chochlik@gmail.com>
2024-06-16 11:44:28 +0200

    Added the examples install component + updated sub-modules

--- config/EAGinePackageOGLplus.cmake
+++ config/EAGinePackageOGLplus.cmake
@@ -6,9 +6,12 @@
 # Package specific options
 #  Debian
 #   Dependencies
-set(CPACK_DEBIAN_OGLPLUS-APPS_PACKAGE_DEPENDS "")
+set(CXX_RUNTIME_PKGS "libc6,libc++1-17")
+set(CPACK_DEBIAN_OGLPLUS-APPS_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd0,zlib1g")
+set(CPACK_DEBIAN_OGLPLUS-EXAMPLES_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd0,zlib1g")
 set(CPACK_DEBIAN_OGLPLUS-DEV_PACKAGE_DEPENDS "eagine-core-dev (>= @EAGINE_VERSION@),eagine-shapes-dev (>= @EAGINE_VERSION@)")
 #   Descriptions
-set(CPACK_DEBIAN_OGLPLUS-APPS_DESCRIPTION "python3,python3-pip")
+set(CPACK_DEBIAN_OGLPLUS-APPS_DESCRIPTION "Collection of OGLplus applications.")
+set(CPACK_DEBIAN_OGLPLUS-EXAMPLESS_DESCRIPTION "EAGine OGLplus examples.")
 set(CPACK_DEBIAN_OGLPLUS-DEV_DESCRIPTION "C++ wrapper for OpenGL.")
 
