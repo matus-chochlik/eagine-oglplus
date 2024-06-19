884ce7be59dd1d096fe3b2f9f422fb42a3f9156f
Matus Chochlik <chochlik@gmail.com>
2024-06-19 11:32:18 +0200

    Changes in how package properties are specified + updated sub-modules

--- config/EAGinePackageOGLplus.cmake
+++ config/EAGinePackageOGLplus.cmake
@@ -2,18 +2,5 @@
 #  Distributed under the Boost Software License, Version 1.0.
 #  See accompanying file LICENSE_1_0.txt or copy at
 #  https://www.boost.org/LICENSE_1_0.txt
-#
-# Package specific options
-#  Debian
-#   Dependencies
-set(CXX_RUNTIME_PKGS "libc6,libc++1-17")
-set(CPACK_DEBIAN_OGLPLUS-APPS_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd0,zlib1g")
-set(CPACK_DEBIAN_OGLPLUS-EXAMPLES_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd0,zlib1g")
-set(CPACK_DEBIAN_OGLPLUS-DEV_PACKAGE_DEPENDS "eagine-core-dev (>= @EAGINE_VERSION@),eagine-shapes-dev (>= @EAGINE_VERSION@)")
-set(CPACK_DEBIAN_OGLPLUS-ASSETS_PACKAGE_DEPENDS "")
-#   Descriptions
-set(CPACK_DEBIAN_OGLPLUS-APPS_DESCRIPTION "Collection of OGLplus applications.")
-set(CPACK_DEBIAN_OGLPLUS-EXAMPLESS_DESCRIPTION "EAGine OGLplus examples.")
-set(CPACK_DEBIAN_OGLPLUS-DEV_DESCRIPTION "C++ wrapper for OpenGL.")
-set(CPACK_DEBIAN_OGLPLUS-ASSETS_DESCRIPTION "Collection of assets usable with the EAGine applications.")
-
+file(APPEND "${CMAKE_BINARY_DIR}/CPackPropertiesEAGine.cmake"
+	"set(EAGINE_OGLPLUS_RUNTIME_PKGS \"libglew2.2,libglfw3\")\n")
